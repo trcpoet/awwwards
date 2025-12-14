0b2f5de3f746f8aaad688f470bec666e18d274b8
Tahsin <tchowdhury29@gmail.com>
2025-12-14 14:36:58 -0600

    Fixed tailwind v3 to v4 codes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="relative h-dvh w-screen overflow-x-hidden">
+            <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden
+            rounded-lg bg-blue-75">
+                <div>
+                    <div className="mask-clip-path">
+
+                    </div>
+
+                </div>
+
+            </div>
+            Hero
+
+        </div>
+    )
+}
+export default Hero
