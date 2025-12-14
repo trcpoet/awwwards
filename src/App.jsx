2d4dc183707fe680958e71557caa75e884e0e167
Tahsin <tchowdhury29@gmail.com>
2025-12-14 13:01:45 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1 className ="text-5xl text-orange-500 font-bold">
+                Welcome to Awwwards
+            </h1>
+        </main>
+
+
+    )
+}
+export default App
