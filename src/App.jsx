0aa22e770406734fa27a91b66af2284768b5a65f
Tahsin <tchowdhury29@gmail.com>
2026-02-01 00:57:27 -0600

    Reconfigured

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,26 @@ import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+  }, []);
+
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <NavBar />
